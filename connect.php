c8ff291200dcefd249e7df1f2b50bc48b7617b6d
Blake Dayman <daymanblake@gmail.com>
2015-04-22 11:59:50 -0700

    finished videos 1-4 created tables created main css index.html and connect.php

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+if ($mysqli->connect_error) {
+	# code...
+	die('Connect Error (' . $mysqli->connect_errno . ')'
+		. $mysqli->connect_error);
+}
+else{
+	echo "Connection made";
+}
+$mysqli->close();
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ToDoList2.0</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<div class="wrap">
+		
+		</div>
+	</body>
+</html>
